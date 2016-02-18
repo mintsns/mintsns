0e1c7cdf259e1c848407780f62c3b5fb4d72633b
y <y-tnb@pm8.jp>
2016-02-18 22:03:39 +0900

    test ui

---
+++ front/lib/js/script.js
@@ -0,0 +1,21 @@
+/**!
+ @license JavaScript for MintSNS by YukiTANABE | pm8.jp
+ */
+(function($){
+ var $window=$(window);
+ var $articleCommentArr = $('article[role=comment]');
+ $window.on({
+  'load': function(){
+   fix();
+  },
+  'resize':function(){
+   fix();
+  }
+ });
+ function fix(){
+  $articleCommentArr.each(function(e){
+       var h = $(this).find('.detail').outerHeight()+$(this).find('.aside').outerHeight();
+       $(this).css({'height':h});
+  });
+ }
+})(jQuery);

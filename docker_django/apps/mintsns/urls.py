6ab86b4782eeb81d2616d323bc2fd138f05c916b
nacika <nacika.inscatolare@gmail.com>
2016-05-08 09:07:47 +0900

    (web) view set

--- docker_django/apps/mintsns/urls.py
+++ docker_django/apps/mintsns/urls.py
@@ -7,26 +7,20 @@
 #     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 # ]
 from django.conf.urls import url, include
-from django.contrib.auth.models import User
+
+# view sets
 from rest_framework import routers, serializers, viewsets
+
+# view set
+from .views import AdminUserViewSet
 
 # Restfulなので本来は必要ないが、検証のため
 from . import views
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
+# TODO: *SECURITY RISK* DELETE
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'admin-users', AdminUserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- docker_django/apps/mintsns/views.py
+++ docker_django/apps/mintsns/views.py
@@ -2,10 +2,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Item
 from redis import Redis
+from rest_framework import viewsets, filters
+from .serializer import AdminUserSerializer
+from django.contrib.auth.models import User as AdminUser
 
 # TODO: grpc
 # from grpc.beta import implementations
 # from protos import customer_service_pb2
+
+# TODO: *SECURITY RISK* DELETE
+# ViewSets define the view behavior.
+class AdminUserViewSet(viewsets.ModelViewSet):
+    queryset = AdminUser.objects.all()
+    serializer_class = AdminUserSerializer
+
 
 redis = Redis(host='redis', port=6379)
 
